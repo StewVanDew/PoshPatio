4a0ed92134243503034a99d511e99d0240125921
Dakota <zeketiki@gmail.com>
2015-07-06 19:38:20 -0700

    Final Update

--- conatct.php
+++ conatct.php
@@ -1,18 +1,24 @@
 <?php
 
-$to = 'email@poshpatio.com';
-$from = $_POST['Email'];
-$name = $_POST['Name'];
+//$to = "Terry@poshpatio.ca"; // required
+$to = "zeketiki@gmail.com"; // required
+$from = "From: Contact@PoshPatio.com";
+$subject = implode(", ", array($_POST["Subject"])); // required
+$Name = $_POST['Name']; // required
+$Email = $_POST['Email']; // required
+$Phone = $_POST['PhoneNumber']; // not required
+$email_message = $_POST['Message']; // required
 
-$subject = $_POST['Subject'];
-$phone = $_POST['PhoneNumber'];
-$message = $_POST['Message'];
+$Subject_message = "Form details below.\n\n";
+$Subject_message .= "Name: " . $Name . "\n";
+$Subject_message .= "Email: " . $Email . "\n";
+$Subject_message .= "Subject: " . $subject . "\n";
+$Subject_message .= "Phone Number: " . $Phone . "\n";
+$Subject_message .= "Message: \n" . $email_message . "\n";
 
-$headers = 'From: ' . $from;
-
-mail($to, $subject, $message, $headers);
-
-$headers = 'From: noreply@poshpatio.com';
-mail($from, 'Message about ' . $subject . 'successfully sent!', 'Thank you for contacting us!', $headers);
-
-header("Location: index.html");
+if (mail($to, $subject, $Subject_message, $from)) {
+    mail($Email, 'Thank you for contacting support!', "We've successfully received your message regarding " . $subject . ". \n We'll do our best to answer your question to the best of our ability. \n Thank you for contacting us!", $from);
+    header("Location: contact.html?done=true");
+} else {
+    header("Location: contact.html?done=false");
+}
